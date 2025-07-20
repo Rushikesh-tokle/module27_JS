1b1bea43d949883529df825764f9e75ee172d72c
rushi <rushikesh.tokle@gmail.com>
2025-07-20 16:28:46 +0530

    API calls through JS made and Fetched Successfully

--- Async.js
+++ Async.js
@@ -1,12 +1,39 @@
-let async={
-    need:"await",
-    name:"Javascript",
-    launch:2014
-}
+// let async={
+//     need:"await",
+//     name:"Javascript",
+//     launch:2014
+// }
 
-let str='{"name":"Rushikesh","roll":54,"age":20,"surname":"Reddy"}';
-let jsObject=JSON.parse(str);
-console.log(jsObject);
+// let str='{"name":"Rushikesh","roll":54,"age":20,"surname":"Reddy"}';
+// let jsObject=JSON.parse(str);
+// console.log(jsObject);
 
-let jsonFormat=JSON.stringify(jsObject);
-console.log(jsonFormat);
+// let jsonFormat=JSON.stringify(jsObject);
+// console.log(jsonFormat);
+
+    let url="http://catfact.ninja/fact";
+    fetch(url)
+    .then((res)=>{
+        // console.log(res);
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data.fact);
+       return fetch(url);
+    })
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data.fact);
+        return fetch(url);
+    })
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+    console.log(data.fact);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
